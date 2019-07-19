9e1811fe9c99ebfbd07a258b041039b39ddb53e0
LIUJO5 <joi.liu@oocl.com>
2019-07-19 15:29:39 +0800

    add Count components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Count/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Count from './components/Count'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Count
   }
 }
 </script>
---
+++ src/components/Count.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <button @click="minusNumber">-</button>
+        <span>  {{count}}  </span>
+        <button @click="addNumber">+</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Count",
+        data() {
+            return {
+                count: 0
+            }
+        },
+        methods: {
+            minusNumber() {
+                this.count--;
+            },
+            addNumber() {
+                this.count++;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
